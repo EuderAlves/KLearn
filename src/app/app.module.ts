67abd746ba5b931b68033282de714e99db34ae35
Euder Alves <euder.alv@gmail.com>
2024-05-21 23:06:05 -0300

    criado componente form-base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { MaterialModule } from './core/material/material.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormBaseComponent } from './shared/form-base/form-base.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginComponent],
+  declarations: [AppComponent, HeaderComponent, LoginComponent, FormBaseComponent],
   imports: [BrowserModule, AppRoutingModule, MaterialModule, HttpClientModule],
   providers: [SwalUtils, provideNgxMask()],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
